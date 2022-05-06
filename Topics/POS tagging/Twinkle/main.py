9375f1d7307e3b10850f72d909559f260066e761
liuyunan <liuyunan.20@gmail.com>
2022-05-05 19:42:01 -0700

    Write a program to find every noun ('NN') in this poem, and print them in order of their appearance, each at a new line.

---
+++ Topics/POS tagging/Twinkle/main.py
@@ -0,0 +1,10 @@
+import nltk
+
+
+poem = ['Twinkle', ',', 'twinkle', ',', 'little', 'star', ',',
+        'How', 'I', 'wonder', 'what', 'you', 'are', '.',
+        'Up', 'above', 'the', 'world', 'so', 'high', ',',
+        'Like', 'a', 'diamond', 'in', 'the', 'sky', '.']
+for word in nltk.pos_tag(poem):
+    if word[1] == 'NN':
+        print(word[0])
