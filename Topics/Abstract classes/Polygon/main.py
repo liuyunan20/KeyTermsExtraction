ea373894896c7af8c9b84310ee524067739fce26
liuyunan <liuyunan.20@gmail.com>
2022-05-10 13:10:34 -0700

    Below you can see the abstract class EquilateralPolygon (it represents geometric shapes where all sides have the same length). Create a subclass Square. Do not forget to override the get_area method (it should return the area of the square).

---
+++ Topics/Abstract classes/Polygon/main.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+
+class EquilateralPolygon(ABC):
+    def __init__(self, side):
+        self.side = side
+
+    @abstractmethod
+    def get_area(self):
+        ...
+
+
+# create Square
+class Square(EquilateralPolygon):
+
+    def get_area(self):
+        return self.side ** 2
