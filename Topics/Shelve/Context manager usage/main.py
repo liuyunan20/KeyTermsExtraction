745adc82294e97ac63b66d6411440fcd1fb9594b
liuyunan <liuyunan.20@gmail.com>
2022-05-11 12:11:40 -0700

    Your task is to open "my_library" using a context manager and add a new book. Use “Anna Karenina” as the key and the string “Happy families are all alike; every unhappy family is unhappy in its own way...” as a value. Then print “A new book has been added to the library!”.

---
+++ Topics/Shelve/Context manager usage/main.py
@@ -0,0 +1,6 @@
+import shelve
+
+
+with shelve.open('my_library', flag='c', writeback=True) as lib:
+    lib['Anna Karenina'] = 'Happy families are all alike; every unhappy family is unhappy in its own way...'
+    print('A new book has been added to the library!')
