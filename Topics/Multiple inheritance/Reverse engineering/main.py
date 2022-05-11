53fb30d48caecf73e1ee03934589bd1c86368c58
liuyunan <liuyunan.20@gmail.com>
2022-05-11 00:02:44 -0700

    define super().__init__()

--- Topics/Multiple inheritance/Reverse engineering/main.py
+++ Topics/Multiple inheritance/Reverse engineering/main.py
@@ -7,28 +7,28 @@ class Vehicle:
 class LandVehicle(Vehicle):
     def __init__(self):
         self.type = 'LandVehicle'
-        super.__init__()
+        super().__init__()
 
 
 class WaterVehicle(Vehicle):
     def __init__(self):
         self.type = 'WaterVehicle'
-        super.__init__()
+        super().__init__()
 
 
 class Car(LandVehicle):
     def __init__(self):
         self.type = 'Car'
-        super.__init__()
+        super().__init__()
 
 
 class Boat(WaterVehicle):
     def __init__(self):
         self.type = 'Boat'
-        super.__init__()
+        super().__init__()
 
 
 class CarBoat(Car, Boat):
     def __init__(self):
         self.type = 'CarBoat'
-        super.__init__()
+        super().__init__()
